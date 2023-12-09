eef82909bc24dfd97de5004c26a4401d1ae07e46
gauravchandrakar25 <gauravchandrakar25@gmail.com>
2023-12-09 23:39:31 +0530

    Contract address owner added in config

--- config/config.ts
+++ config/config.ts
@@ -2,3 +2,4 @@ exports.rpcUrl =
   "https://polygon-mumbai.infura.io/v3/49da1f1e1162470eac362186f2ddf558";
 exports.CouponTokenContractAddress =
   "0x0d83AD0aDb21ea8e8f336f625A9485Ad91fe5668";
+exports.CouponTokenOwnerAddress = "0x74Ecafd52EfE88897D8571EAAD2fF20bC312e933";
